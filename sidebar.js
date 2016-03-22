45c3df5eb6d9a4f15919e3b82bf44a7f780a226b
Rishi Patel <rishi@docnavlab.com>
2016-03-22 18:14:39 -0400

    generally working well now, need to add open / close

--- sidebar.js
+++ sidebar.js
@@ -11,16 +11,41 @@ $(document).ready(function() {
     // $('head').append('<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">');
 
     // Container for siebar html
-    debugger;
     var sidebarFrame = $("<div></div>").attr({'id' : 'outliner-sidebar-frame'});
     var sidebarContents = $("<div></div>").attr({'id' : 'outliner-sidebar-contents'});
     sidebarFrame.append(sidebarContents);
 
     var headerTags = $("h1, h2, h3, h4, h5, h6");
+    var sidebarLinks = [];
     for (var i = 0; i < headerTags.length; i++) {
+
         header = headerTags.eq(i);
-        headerCopy = header.clone();
-        sidebarContents.append(headerCopy);
+
+        // grab or add an id for the header
+        headerID = header.attr('id');
+        if (headerID === undefined) {
+            // headerText = header.contents().eq(0).text().trim();
+            headerText = header.text().trim();
+            console.log(headerText);
+            headerID = headerText.replace(/ /g,'_');
+            header.attr('id', headerID);
+        }
+
+        // create new link to the
+        sideBarHeader = $(
+            "<" + header.prop('tagName') + ">",
+            {
+                id: headerID,
+                text: headerText,
+                class: "sidebar-header"
+            }
+        );
+        sidebarLink = $("<a>", {href: "#" + headerID});
+
+        // append onto side panel
+        sidebarLink.append(sideBarHeader);
+        sidebarContents.append(sidebarLink);
+
     }
 
     $('body').append(sidebarFrame);
