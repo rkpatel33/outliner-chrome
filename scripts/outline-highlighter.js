94bea8cafe0dae89ad60de5568bd005cedf920fe
Rishi Patel <mail@rishinyc.com>
2019-02-01 10:37:48 -0800

    bugfix

--- scripts/outline-highlighter.js
+++ scripts/outline-highlighter.js
@@ -6,14 +6,16 @@ function isElementInViewport(el) {
         el = el[0];
     }
 
-    var rect = el.getBoundingClientRect();
-
-    return (
-        rect.top >= 0 &&
+    if (el) {
+        var rect = el.getBoundingClientRect();
+        return (
+            rect.top >= 0 &&
             rect.left >= 0 &&
             rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && /* or $(window).height() */
             rect.right <= (window.innerWidth || document.documentElement.clientWidth) /* or $(window).width() */
-    );
+        );
+    }
+
 }
 
 var updateOutline = () => {
@@ -38,4 +40,3 @@ var updateOutline = () => {
 };
 
 $(window).on('DOMContentLoaded load resize scroll', updateOutline);
-
